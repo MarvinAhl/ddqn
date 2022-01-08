8fbb579c1aab49fedc6b9a63e42a83fc00b53637
MarvinAhl <marvin_ahl@t-online.de>
2022-01-08 14:54:18 +0100

    Made ddqn_lunar_lander.py train infinitely

--- ddqn_lunar_lander.py
+++ ddqn_lunar_lander.py
@@ -7,7 +7,6 @@ from tqdm import tqdm
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-episodes = 100
 backup_episodes = 10
 
 env = gym.make('LunarLander-v2')
@@ -15,13 +14,12 @@ env = gym.make('LunarLander-v2')
 nS = env.observation_space.shape[0]
 nA = env.action_space.n
 
-agent = DDQN(nS, nA, hidden_layers=(500, 500, 500), gamma=0.99, learning_rate_start=0.0003, learning_rate_decay_steps=1, learning_rate_min=0.0003,
-             epsilon_start=0.1, epsilon_decay_steps=1, epsilon_min=0.1, buffer_size_min=200, buffer_size_max=50000, batch_size=50, replays=1, tau=0.01, device=device)
-agent.load_net('lunar_lander_a2_r3.net')
+agent = DDQN(nS, nA, hidden_layers=(500, 500, 500), gamma=0.99, learning_rate_start=0.0005, learning_rate_decay_steps=20000, learning_rate_min=0.0003,
+             epsilon_start=1.0, epsilon_decay_steps=20000, epsilon_min=0.1, buffer_size_min=200, buffer_size_max=50000, batch_size=50, replays=1, tau=0.01, device=device)
 
-episode_rewards = np.zeros(episodes, dtype=np.float32)
+episode_rewards = []
 step_count = 0
-steps = np.zeros(episodes, dtype=np.int32)
+steps = []
 
 
 def save_stats(curr_episode):
@@ -30,26 +28,29 @@ def save_stats(curr_episode):
     global episode_rewards
     global steps
 
-    agent.save_net('lunar_lander_a2_r3_v2.net')
+    agent.save_net('lunar_lander.net')
 
     plt.clf()  # To prevent overlapping of old plots
 
     plt.subplot(211)
-    plt.plot(np.arange(curr_episode), episode_rewards[:curr_episode], 'k-')
+    plt.plot(np.arange(curr_episode) + 1, episode_rewards, 'k-')
     plt.grid(True)
     plt.title('Training Stats')
     plt.ylabel('Reward')
 
     plt.subplot(212)
-    plt.plot(np.arange(curr_episode), steps[:curr_episode], 'k-')
+    plt.plot(np.arange(curr_episode), steps, 'k-')
     plt.grid(True)
     plt.xlabel('Episodes')
     plt.ylabel('Steps')
 
     plt.savefig('rewards_train.png')
 
+    print(f'Episode {curr_episode} saved')
 
-for episode in tqdm(range(episodes)):
+episode = 0
+while True:
+    episode += 1
     obsv, done = env.reset(), False
 
     episode_reward = 0.0
@@ -69,12 +70,12 @@ for episode in tqdm(range(episodes)):
         episode_reward += reward  # For statistics
         step_count += 1
     
-    episode_rewards[episode] = episode_reward
-    steps[episode] = step_count
+    episode_rewards.append(episode_reward)
+    steps.append(step_count)
 
     if episode % backup_episodes == 0:
-        save_stats(episode+1)
+        save_stats(episode)
 
 env.close()
 
-save_stats(episodes)
+save_stats(episode)
